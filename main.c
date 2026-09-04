14b7ed2d15fb7776cf369aa86f0f9dc869979065
unknown <arthur.f.campos@ufv.br>
2026-09-04 10:47:45 -0300

    correcoes main.c

--- main.c
+++ main.c
@@ -21,34 +21,28 @@ int main(){
         printf("\nEscolha o tipo de figura basica a ser usada para criar a obra:\n");
 
         if (existe_ultimo_quadro){
-            printf("\n0 - repetir ultimo quadro\n");
-            printf("\n");
+            printf("\n0 - repetir ultimo quadro\n\n");
         }
 
         printf("1 - asterisco simples\n");
         printf("2 - simbolo de soma com asteriscos.\n");
         printf("3 - letra X com asteriscos.\n");
         printf("4 - figuras aleatorias\n");
-
         printf("5 - ver obra de arte criativa\n");
-
-        printf("6 - encerrar o programa\n");
-
-        printf("\n");
+        printf("6 - encerrar o programa\n\n");
     
         scanf("%d", &controle_switch);
 
         if (controle_switch == 6){
-            sair = 1;
-            continue;
+            sair = 1; continue;
         }
 
         int repetindo = 0;
 
         if (controle_switch == 0){
             if (existe_ultimo_quadro == 0){
-                controle_switch = 4;
-                printf("\nopcao invalida. ativando geracao de figuras aleatorias\n");
+                controle_switch = 5;
+                printf("\nopcao invalida. ativando obra criativa\n");
             }
         
             else{
@@ -60,8 +54,8 @@ int main(){
 
         if (repetindo == 0){
             if ((controle_switch < 1 || controle_switch > 5)){
-                controle_switch = 4;
-                printf("\nopcao invalida. ativando geracao de figuras aleatorias\n");
+                controle_switch = 5;
+                printf("\nopcao invalida. ativando obra criativa\n");
             }
 
             printf("\ndigite a quantidade de figuras desejadas:\n");
